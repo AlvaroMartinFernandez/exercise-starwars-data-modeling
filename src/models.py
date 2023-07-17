6291bd187b93023a2075f878dadd71ac57bc3886
Alvaro Martin Fernandez <91843474+AlvaroMartinFernandez@users.noreply.github.com>
2023-07-17 15:22:35 +0000

    modelo usuario y favoritos

--- src/models.py
+++ src/models.py
@@ -11,33 +11,9 @@ class Peoples(Base):
     __tablename__ = 'peoples'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    uid = Column(String(250), primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     url = Column(String(250), nullable=False)
-    
-    
-class Vehicles(Base):
-    __tablename__ = 'vehicles'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    uid = Column(String(250), primary_key=True)
-    name = Column(String(250), nullable=False)
-    url = Column(String(250), nullable=False)
-class Planets(Base):
-    __tablename__ = 'planets'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    uid = Column(String(250), primary_key=True)
-    name = Column(String(250), nullable=False)
-    url = Column(String(250), nullable=False)
-
-
-class PropertiesPeople(Base):
-    __tablename__ = 'propertiespeople'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(String(250), primary_key=True)
-    peoples_uid = Column(String(250), ForeignKey('peoples.uid'))
     height=Column(String(250), nullable=False)
     mass=Column(String(250), nullable=False)
     hair_color= Column(String(250), nullable=False)
@@ -48,16 +24,15 @@ class PropertiesPeople(Base):
     created=Column(DATETIME, nullable=False)
     edited= Column(DATETIME, nullable=False)
     homeworld=Column(String(250), nullable=False)
+    
 
-
-
-class PropertiesVehicle(Base):
-    __tablename__ = 'propertiesvehicle'
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(String(250), primary_key=True)
-    vehicles_uid = Column(String(250), ForeignKey('vehicles.uid'))
-
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
     model= Column(String(250), nullable=False)
     vehicle_class=Column(String(250), nullable=False)
     manufacturer = Column(String(250), nullable=False)
@@ -73,13 +48,13 @@ class PropertiesVehicle(Base):
     edited= Column(DATETIME, nullable=False)
 
 
-class PropertiesPlanet(Base):
-    __tablename__ = 'propertiesplanet'
+class Planets(Base):
+    __tablename__ = 'planets'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(String(250), primary_key=True)
-    vehicles_uid = Column(String(250), ForeignKey('planets.uid'))
-
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
     diameter= Column(String(250), nullable=False)
     rotation_period=Column(String(250), nullable=False)
     orbital_period = Column(String(250), nullable=False)
@@ -90,6 +65,30 @@ class PropertiesPlanet(Base):
     surface_water = Column(String(250), nullable=False)
     created=Column(DATETIME, nullable=False)
     edited= Column(DATETIME, nullable=False)
+
+class Users(Base):
+    __tablename__ = 'users'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    created = Column(String(250), nullable=False)
+    edited = Column(String(250), nullable=False)
+
+
+
+class ListFavorites(Base):
+    __tablename__ = 'listfavorites'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id=Column(Integer, ForeignKey("users.id"), nullable=False)
+    peoples_id=Column(Integer, ForeignKey("peoples.id"), nullable=False)
+    vehicles_id=Column(Integer, ForeignKey("vechicles.id"), nullable=False)
+    planets_id=Column(Integer, ForeignKey("planets.id"), nullable=False)
+
 
 
 
